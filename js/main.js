08817193a6ad91ef04e05749aaad1f7209f5abc6
Susanto <nerdyspook007@gmail.com>
2021-12-26 00:10:21 +0530

    added scrollup and scrollbar

--- js/main.js
+++ js/main.js
@@ -77,3 +77,16 @@ function scrollActive() {
     });
 }
 window.addEventListener("scroll", scrollActive);
+
+/* ================= Show Scroll Up ================== */
+function scrollUp() {
+    const scrollUp = document.querySelector("#scroll-up");
+
+    if (this.scrollY >= 350) {
+        scrollUp.classList.add("show-scroll");
+    } else {
+        scrollUp.classList.remove("show-scroll");
+    }
+}
+
+window.addEventListener("scroll", scrollUp);
