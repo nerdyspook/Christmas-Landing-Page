774d726cebc9e0f780417037f69fadf0bbf04eaf
Susanto <nerdyspook007@gmail.com>
2021-12-25 20:39:07 +0530

    added swiper bundle

--- js/main.js
+++ js/main.js
@@ -34,3 +34,22 @@ function scrollHeader() {
 }
 
 window.addEventListener("scroll", scrollHeader);
+
+/* =========== New Swiper =========== */
+let newSwiper = new Swiper(".new-swiper", {
+    spaceBetween: 24,
+    loop: "true",
+    slidesPerView: "auto",
+    centeredSlides: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+
+    breakpoints: {
+        992: {
+            spaceBetween: 80,
+        },
+    },
+});
