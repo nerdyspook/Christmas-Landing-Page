29ff1ca8396b46a40ca88f4b3ff617bcdf391c4b
Susanto <nerdyspook007@gmail.com>
2021-12-26 11:49:29 +0530

    added animations

--- js/main.js
+++ js/main.js
@@ -128,3 +128,20 @@ themeButton.addEventListener("click", () => {
     localStorage.setItem("selected-theme", getCurrentTheme());
     localStorage.setItem("selected-icon", getCurrentIcon());
 });
+
+/* ==================== Scroll Reveal Animations ===================== */
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "60px",
+    duration: 2500,
+    delay: 400,
+    // reset: true
+});
+
+sr.reveal(`.home__img, .new__container, .footer__container`);
+sr.reveal(`.home__data`, { delay: 500 });
+sr.reveal(`.giving__content, .gift__card`, { interval: 100 });
+sr.reveal(`.celebrate__data, .message__form, .footer__img1`, {
+    origin: "left",
+});
+sr.reveal(`.celebrate__img, .message__img, .footer__img2`, { origin: "right" });
